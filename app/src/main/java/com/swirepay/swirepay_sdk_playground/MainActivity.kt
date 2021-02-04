61170c271b3f72ce12ff3423a47c4de62e21c50b
sivarooban@omegaitr.com <sivarooban@omegaitr.com>
2021-02-04 14:56:25 +0530

    first commit

---
+++ app/src/main/java/com/swirepay/swirepay_sdk_playground/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.swirepay.swirepay_sdk_playground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
