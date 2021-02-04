b20600690d5907c610b0c39d151a476d4af9e34c
sivarooban@omegaitr.com <sivarooban@omegaitr.com>
2021-02-04 15:42:34 +0530

    sdk inclucded

---
+++ android_sdk/src/main/java/com/swirepay/android_sdk/PaymentActivity.kt
@@ -0,0 +1,36 @@
+package com.swirepay.android_sdk
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import com.swirepay.android_sdk.databinding.ActivityMainBinding
+
+private class PaymentActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.webView.let {
+            it.loadUrl("https://secure.swirepay.com/payment-link/paymentlink-76eff35306c244e6a89632c77df9796e")
+            it.settings.javaScriptEnabled = true
+            it.webViewClient  = object : WebViewClient(){
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    request?.let {
+                        val redirectionUrl = request!!.url.toString()
+                        // TODO Check the status here (api)
+//                      val param = Uri.parse(url.toString()).getQueryParameter("zx")
+                    }
+                    //TODO return the result here
+                    return true
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/swirepay/swirepay_sdk_playground/MainActivity.kt
+++ app/src/main/java/com/swirepay/swirepay_sdk_playground/MainActivity.kt
@@ -2,10 +2,12 @@ package com.swirepay.swirepay_sdk_playground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.swirepay.android_sdk.SwirepaySdk
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val sdk = SwirepaySdk().secretKey
     }
 }
